a8fc9632e04c8a7ad664ccf68d891fc27c4413a2
markwelshboy <mark.david.richards@gmail.com>
2026-09-16 16:24:42 -0700

    Enable snapshot ID list display

--- bin/pod_snapshot_env_cli.py
+++ bin/pod_snapshot_env_cli.py
@@ -6,10 +6,12 @@ import sys
 import pod_env_state
 import pod_history_state
 import pod_snapshot_cli
+import pod_snapshot_display
 import pod_state as core
 
 pod_env_state.install_core_hooks(core)
 pod_history_state.install_core_hooks(core)
+pod_snapshot_display.install_cli_hooks(pod_snapshot_cli)
 
 
 if __name__ == "__main__":
