b141d564306a9cb6c8a372d09528824735c877d4
markwelshboy <mark.david.richards@gmail.com>
2026-09-17 23:13:04 -0700

    Remove obsolete timezone import

--- bin/rpods.py
+++ bin/rpods.py
@@ -7,7 +7,7 @@ import shlex
 import sys
 import urllib.request
 from dataclasses import dataclass
-from datetime import datetime, timezone
+from datetime import datetime
 from pathlib import Path
 from typing import Any
 
